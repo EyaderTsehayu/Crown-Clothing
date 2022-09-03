a60ecc7dfd0666ca68f375a0c627139b5e24b0cb
Eyader Tsehayu <eyaderts@gmail.com>
2022-09-03 23:08:47 +0300

    update sign-in routes for signUpForm

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,17 +3,21 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils.js";
+
+import "../../components/sign-up-form/sign-up-form.component";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-    //console.log(response);
   };
 
   return (
     <div>
       <h1>sign in page</h1>
       <button onClick={logGoogleUser}>Sign in With google popup</button>
+      <SignUpForm />
     </div>
   );
 };
