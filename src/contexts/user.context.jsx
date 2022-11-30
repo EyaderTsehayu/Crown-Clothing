6abdbbc37fd925ee9716350cefd4f2f6db637715
Eyader Tsehayu <eyaderts@gmail.com>
2022-11-30 10:38:38 +0300

    change dispatch with helper function createAction

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -4,6 +4,7 @@ import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 //as the actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
@@ -15,8 +16,8 @@ export const USER_ACTION_TYPES = {
 };
 
 const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
+  //console.log("dispatched");
+  // console.log(action);
 
   const { type, payload } = action;
 
@@ -41,7 +42,7 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
